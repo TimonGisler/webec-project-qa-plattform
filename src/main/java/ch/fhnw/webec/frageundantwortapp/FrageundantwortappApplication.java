068bde6ac6bbec672c042901bcb4a2c7d45937bf
Your Name <timongisler@icloud.com>
2024-04-18 17:57:01 +0200

    generated spring boot project

---
+++ src/main/java/ch/fhnw/webec/frageundantwortapp/FrageundantwortappApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.webec.frageundantwortapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrageundantwortappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrageundantwortappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/fhnw/webec/frageundantwortapp/FrageundantwortappApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.webec.frageundantwortapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrageundantwortappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
