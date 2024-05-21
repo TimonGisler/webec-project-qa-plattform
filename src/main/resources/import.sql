798ea396b8f0e933156b7ae1052a70c32a3c9265
Your Name <timongisler@icloud.com>
2024-05-21 21:26:38 +0200

    added some default questions for testing purposes

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO QUESTION (id, title, text) VALUES (1, 'Some Title?', 'Description of problem?');
+INSERT INTO QUESTION (id, title, text) VALUES (2, 'Other Title', 'Other description of problem?');
