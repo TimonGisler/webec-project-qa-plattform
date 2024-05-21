caed0c666376a549682d848fb1ef5a928f5b8cf0
Your Name <timongisler@icloud.com>
2024-05-21 22:07:51 +0200

    Prevent unique constraint collisions

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
-INSERT INTO QUESTION (id, title, text) VALUES (1, 'Some Title?', 'Description of problem?');
-INSERT INTO QUESTION (id, title, text) VALUES (2, 'Other Title', 'Other description of problem?');
+/*TODO TGIS, find permanent solution for ids, otherwise sooner or later there will probably be a collision*/
+INSERT INTO QUESTION (id, title, text) VALUES (9998, 'Some Title?', 'Description of problem?');
+INSERT INTO QUESTION (id, title, text) VALUES (9999, 'Other Title', 'Other description of problem?');
+
+
