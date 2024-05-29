6f394ebe6e4a25882594a39a5b7dec58d143a64e
Your Name <timongisler@icloud.com>
2024-05-29 17:13:11 +0200

    generate id instead of hardcoding it

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,7 @@
-/*TODO TGIS, find permanent solution for ids, otherwise sooner or later there will probably be a collision*/
-INSERT INTO QUESTION (id, title, text) VALUES (9998, 'Some Title?', 'Description of problem?');
-INSERT INTO QUESTION (id, title, text) VALUES (9999, 'Other Title', 'Other description of problem?');
+INSERT INTO QUESTION (id, title, text) VALUES (NEXT VALUE FOR QUESTION_SEQ, 'Some Title?', 'Description of problem?');
+INSERT INTO QUESTION (id, title, text) VALUES (NEXT VALUE FOR QUESTION_SEQ, 'Other Title', 'Other description of problem?');
 
-INSERT INTO TAG (id, name) VALUES (9997, 'Java');
-INSERT INTO TAG (id, name) VALUES (9998, 'Spring');
-INSERT INTO TAG (id, name) VALUES (9999, 'HTML');
+INSERT INTO TAG (id, name) VALUES (NEXT VALUE FOR TAG_SEQ, 'Java');
+INSERT INTO TAG (id, name) VALUES (NEXT VALUE FOR TAG_SEQ, 'Spring');
+INSERT INTO TAG (id, name) VALUES (NEXT VALUE FOR TAG_SEQ, 'HTML');
 
