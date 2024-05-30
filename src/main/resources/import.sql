c965ce4dc4723bcd767f44d1759213b508d7a304
Your Name <timongisler@icloud.com>
2024-05-30 12:39:37 +0200

    added some more start data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,18 @@ INSERT INTO TAG (id, name) VALUES (NEXT VALUE FOR TAG_SEQ, 'Java');
 INSERT INTO TAG (id, name) VALUES (NEXT VALUE FOR TAG_SEQ, 'Spring');
 INSERT INTO TAG (id, name) VALUES (NEXT VALUE FOR TAG_SEQ, 'HTML');
 
+-- Associate the first question with the "Java" and "Spring" tags
+INSERT INTO QUESTION_TAGS (QUESTION_ID, TAGS_ID) VALUES ((SELECT id FROM QUESTION WHERE title = 'Some Title?'), (SELECT id FROM TAG WHERE name = 'Java'));
+INSERT INTO QUESTION_TAGS (QUESTION_ID, TAGS_ID) VALUES ((SELECT id FROM QUESTION WHERE title = 'Some Title?'), (SELECT id FROM TAG WHERE name = 'Spring'));
+
+-- Associate the second question with the "HTML" tag
+INSERT INTO QUESTION_TAGS (QUESTION_ID, TAGS_ID) VALUES ((SELECT id FROM QUESTION WHERE title = 'Other Title'), (SELECT id FROM TAG WHERE name = 'HTML'));
+
+-- Add some answers to the first question
+-- Add answers to the first question
+INSERT INTO ANSWER (id, text) VALUES (NEXT VALUE FOR ANSWER_SEQ, 'This is the first answer to the first question.');
+INSERT INTO ANSWER (id, text) VALUES (NEXT VALUE FOR ANSWER_SEQ, 'This is the second answer to the first question.');
+
+-- Associate the answers with the first question
+INSERT INTO QUESTION_ANSWERS (QUESTION_ID, ANSWERS_ID) VALUES ((SELECT id FROM QUESTION WHERE title = 'Some Title?'), (SELECT id FROM ANSWER WHERE text = 'This is the first answer to the first question.'));
+INSERT INTO QUESTION_ANSWERS (QUESTION_ID, ANSWERS_ID) VALUES ((SELECT id FROM QUESTION WHERE title = 'Some Title?'), (SELECT id FROM ANSWER WHERE text = 'This is the second answer to the first question.'));
